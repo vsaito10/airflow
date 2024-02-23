b22c77c91eb4921ffeab651546a567901f7a290a
Vitor Saito <vitorsaito95@gmail.com>
2024-02-23 15:05:27 -0300

    atualizando o código para pegar automaticamente o link da planilha mais atual

--- dags/proxy_rate_dag.py
+++ dags/proxy_rate_dag.py
@@ -1,8 +1,10 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
+from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import os
+import re
 import requests
 
 
@@ -17,8 +19,8 @@ def download_arquivo():
     # Diretório do download do arquivo
     download_directory = 'csv_tratados'
 
-    # URL do download do arquivo. Esse link é do site https://www.frbsf.org/economic-research/indicators-data/proxy-funds-rate/
-    url = 'https://www.frbsf.org/wp-content/uploads/sites/4/proxy-funds-rate-data.xlsx?202305052'
+    # Site do FED San Francisco
+    url = 'https://www.frbsf.org/research-and-insights/data-and-indicators/proxy-funds-rate/'
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
@@ -28,15 +30,34 @@ def download_arquivo():
         'Connection': 'close'
     }
 
+    # Solicitando o HTTP GET do site do FED San Francisco
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        # Obtém o nome do arquivo sem a parte da URL após o "?" -> 'proxy-funds-rate-data.xlsx'
-        nome_arquivo = os.path.basename(url).split("?")[0]
+    # HTML da página
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Usando o regex para encontrar o link da planilha
+    link_pattern = re.compile(r'/wp-content/uploads/proxy-funds-rate-data\.xlsx\?(\d+)')
+    link = soup.find('a', href=link_pattern)
+
+    # Selecionando o href -> '/wp-content/uploads/proxy-funds-rate-data.xlsx?20240105'
+    href = link.get('href')
+
+    # URL do download da planilha do Proxy Rate
+    url_planilha = f'https://www.frbsf.org/{href}'
+
+    # Fazendo o donwload da planilha do Proxy Rate
+    response_planilha = requests.get(url_planilha, headers=headers)
+
+    if response_planilha.status_code == 200:
+        nome_arquivo = os.path.basename(url_planilha).split("?")[0]  # Obtém o nome do arquivo sem a parte da URL após o "?" -> 'proxy-funds-rate-data.xlsx'
         nome_arquivo = os.path.join(download_directory, nome_arquivo)
         with open(nome_arquivo, 'wb') as f:
-            f.write(response.content)
+            f.write(response_planilha.content)
             print("Arquivo baixado com sucesso.")
+
+    else:
+        print('Arquivo não encontrado.')
 
 
 with DAG(
